35bdd3c7e235c8679372badc9284eedb995e7b61
xiaohuoni <448627663@qq.com>
2021-09-10 15:57:40 +0800

    first commit

---
+++ packages/hello/src/index.test.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { render, testA11y } from '@alita/test';
+import Hello from './index';
+
+it('passes a11y test', async () => {
+  await testA11y(<Hello />);
+});
+
+test('render', () => {
+  const { getByText } = render(<Hello />);
+  expect(getByText('hello')).toBeDefined()
+})
